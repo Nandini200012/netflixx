68bd319bfa73e8469d8390d30a582e8e367ecf29
Nandini <nandhininatarajan04@gmail.com>
2023-06-12 10:31:50 +0530

    Netflix

---
+++ lib/widgets/main_title.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MainTitle extends StatelessWidget {
+  final String title;
+  const MainTitle({super.key,required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Text(
+      title,
+      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    );
+  }
+}
